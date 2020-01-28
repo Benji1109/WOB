123bc731ea72ef74e67a4ccab54730b765ee3353
Benjamin Burka <benjamin.burka@suitsolutions.eu>
2020-01-28 21:03:41 +0100

    init project

---
+++ src/main/java/hu/wob/wobrest/DemoApplication.java
@@ -0,0 +1,13 @@
+package hu.wob.wobrest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import hu.wob.wobrest.controller.StartUpInit;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
